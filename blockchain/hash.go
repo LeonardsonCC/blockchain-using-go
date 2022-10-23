a492d0d70fe3ec286f636118af1e8077c685d64a
LeonardsonCC <leonardson.carvalho@gmail.com>
2022-10-22 22:11:40 -0300

    refactor: changed package name

--- blockchain/hash.go
+++ blockchain/hash.go
@@ -1,4 +1,4 @@
-package block
+package blockchain
 
 type Address string
 type Hash string
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/LeonardsonCC/blockchain-using-go/block"
+	"github.com/LeonardsonCC/blockchain-using-go/blockchain"
 )
 
 func main() {
@@ -12,11 +12,11 @@ func main() {
 
 	moment := time.Date(2022, 10, 22, 20, 12, 0, 0, time.Now().Location())
 
-	bc := block.NewBlockchain(moment, 5)
+	bc := blockchain.NewBlockchain(moment, 5)
 
-	bc.AddTx(moment, block.NewData("TEST-1", "TEST-2", 100))
-	bc.AddTx(moment, block.NewData("TEST-2", "TEST-3", 200))
-	bc.AddTx(moment, block.NewData("TEST-3", "TEST-1", 300))
+	bc.AddTx(moment, blockchain.NewData("TEST-1", "TEST-2", 100))
+	bc.AddTx(moment, blockchain.NewData("TEST-2", "TEST-3", 200))
+	bc.AddTx(moment, blockchain.NewData("TEST-3", "TEST-1", 300))
 
 	log.Printf("Blockchain: %s\n", bc.ToString())
 
